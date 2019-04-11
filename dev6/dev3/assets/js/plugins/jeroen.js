9a2e1877db6436b604f2c5f53a7e105bd97aa9ae
Jeroen Kuik <jmkuik@gmail.com>
2019-04-11 16:18:47 +0200

    random updates

--- dev6/dev3/assets/js/plugins/jeroen.js
+++ dev6/dev3/assets/js/plugins/jeroen.js
@@ -1,70 +1,91 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userInput");
-var ul = document.querySelector("ulist")
-var deleteBtns = document.getElementsByClassName("delete");
-var items = ul.getElementsByTagName("li");
+// var button = document.getElementById("enter");
+// var input = document.getElementById("userInput");
+// var ul = document.querySelector("ulist")
+// var deleteBtns = document.getElementsByClassName("delete");
+// var items = ul.getElementsByTagName("li");
 
 
 
-//add event listener to first 6 btns in HTML file
-for(var i = 0; i < deleteBtns.length; i++){
-	deleteBtns[i].addEventListener("click", removeParent, false);
+// //add event listener to first 6 btns in HTML file
+// for(var i = 0; i < deleteBtns.length; i++){
+// 	deleteBtns[i].addEventListener("click", removeParent, false);
+// }
+
+
+// //from StackOverflow:
+// function removeParent(evt) {
+//   evt.target.removeEventListener("click", removeParent, false);
+//   evt.target.parentNode.remove();
+// }
+
+// //click on a list item and it strikethroughs the text
+// function getEventTarget(e){
+// 	e = e || window.event;
+// 	return e.target || e.srcElement;
+// }
+
+
+// ul.onclick = function(event){
+// 	var target = getEventTarget(event);
+// 	target.classList.toggle("done");
+// }
+
+
+// //adding new items:
+
+// function inputLength(){
+// 	return input.value.length;
+// }
+
+// function createListElement() {
+// 	var btn = document.createElement("button");
+// 	btn.innerHTML = "Delete";
+// 	btn.onclick = removeParent;
+
+// 	var li = document.createElement("li");
+// 	li.appendChild(document.createTextNode(input.value));
+// 	li.innerHTML = li.innerHTML + " ";
+// 	li.appendChild(btn);
+
+// 	ul.appendChild(li);
+// 	input.value="";
+// }
+
+
+// function addToListAfterClick(){
+// 	if(inputLength() > 0){
+// 			createListElement();
+// 		}
+// }
+
+// function addToListAfterKeypress(event){
+// 	if(inputLength() > 0 && event.keyCode === 13) {
+// 		createListElement();
+// 	}
+// }
+
+
+// button.addEventListener("click", addToListAfterClick);
+
+// input.addEventListener("keypress", addToListAfterKeypress);
+
+// Background Generator
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
 }
 
+color1.addEventListener("input", setGradient);
 
-//from StackOverflow:
-function removeParent(evt) {
-  evt.target.removeEventListener("click", removeParent, false);
-  evt.target.parentNode.remove();
-}
-
-//click on a list item and it strikethroughs the text
-function getEventTarget(e){
-	e = e || window.event;
-	return e.target || e.srcElement;
-}
-
-
-ul.onclick = function(event){
-	var target = getEventTarget(event);
-	target.classList.toggle("done");
-}
-
-
-//adding new items:
-
-function inputLength(){
-	return input.value.length;
-}
-
-function createListElement() {
-	var btn = document.createElement("button");
-	btn.innerHTML = "Delete";
-	btn.onclick = removeParent;
-
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	li.innerHTML = li.innerHTML + " ";
-	li.appendChild(btn);
-
-	ul.appendChild(li);
-	input.value="";
-}
-
-
-function addToListAfterClick(){
-	if(inputLength() > 0){
-			createListElement();
-		}
-}
-
-function addToListAfterKeypress(event){
-	if(inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
-}
-
-
-button.addEventListener("click", addToListAfterClick);
-
-input.addEventListener("keypress", addToListAfterKeypress);
+color2.addEventListener("input", setGradient);
